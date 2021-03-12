a6ca4188db5f4f91b4a2dbaa50801b3325f67671
dish59742 <77671863+dish59742@users.noreply.github.com>
2021-03-12 13:19:21 +0530

    Update .manylinux-install.sh

--- .manylinux-install.sh
+++ .manylinux-install.sh
@@ -10,6 +10,7 @@ for PYBIN in /opt/python/*/bin; do
        [[ "${PYBIN}" == *"cp37"* ]] || \
        [[ "${PYBIN}" == *"cp38"* ]]; then
         "${PYBIN}/pip" install -e /io/
+	"${PYBIN}/pip" install wheel cffi-dev
         "${PYBIN}/pip" wheel /io/ -w wheelhouse/
 	rm -rf /io/build /io/*.egg-info
     fi
